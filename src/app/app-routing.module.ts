1321fd9e91190355d4a4776748d57cc4ab55b166
lulukarissa <lulukarissa@gmail.com>
2019-03-19 20:37:29 +0700

    service

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
+// import { Routes, RouterModule } from '@angular/router';
+
+// const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    // RouterModule.forRoot(routes)
+    CommonModule
+  ],
+  declarations: []
+  // exports: [RouterModule]
+})
+export class AppRoutingModule { }
